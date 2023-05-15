062f3ef8450b5f76a558e815280cc85945befad3
tamerlankayak <tamerlan_94@mail.ru>
2023-05-15 22:59:36 +0400

    some comments

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
-
+	//declaration of port
 	router.Run("localhost:8080")
 }
 
+// creating models
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -21,12 +22,14 @@ type album struct {
 }
 
 // albums slice to seed record album data.
+// listing all data to albums
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+// getting all data via http
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
